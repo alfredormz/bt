4ff95eaa5740aa788220d93adcd680bb5ba658d8
Alfredo Ramirez <alfredormz@gmail.com>
2016-08-01 12:11:06 -0400

    Change the default root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'dashboard#index'
 	resources :bars, only: [:index]
   get 'angular_test', to: "angular_test#index"
+
+  root 'bars#index'
 end
